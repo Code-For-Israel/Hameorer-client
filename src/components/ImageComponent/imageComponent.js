3f649d2f394df0214146848731644662bacd4509
itzikd1 <itzikd1@gmail.com>
2022-11-29 00:43:54 +0200

    image component can upload multi file but can't preview

--- src/components/ImageComponent/imageComponent.js
+++ src/components/ImageComponent/imageComponent.js
@@ -1,36 +1,45 @@
 import {Button, View, Image} from "react-native";
 import * as React from "react";
+import _ from "lodash"; // cool kids know _ is low-dash
 
 export function ImageComponent() {
 
-    const [photo, setPhoto] = React.useState(null);
+    const [photos, setPhotoss] = React.useState(null);
     const [photoUri, setPhotoUri] = React.useState(null);
+    const photoList = []
 
     async function uploadFile() {
-        if (photo == null) return;
-        const fileUrl = URL.createObjectURL(photo)
-        setPhotoUri(fileUrl)
+        if (photos == null) return;
+        Array.from(photos).forEach(photo => {
+            const photoUrl = URL.createObjectURL(photo);
+            const photoName = photo.name;
+            let deepCopyPhoto = _.cloneDeep({name: photoName, uri: photoUrl});
+            photoList.push(deepCopyPhoto)
+        });
 
-        //create form data
-        const data = new FormData();
-        data.append('name', photo.name);
-        data.append('type', photo.type);
-        data.append('uri', fileUrl);
+        // create form data
+        // const data = new FormData();
+        // data.append('name', photos.name);
+        // data.append('type', photos.type);
+        // data.append('uri', fileUrl);
+        console.log(photoList)
     }
 
     return (
         <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
             <>
-                {photo ? (
+                {photoList && photoList.length>0 ? photoList.forEach((img) =>
                     <Image
-                        source={{uri: photoUri}}
+                        source={{uri: img.uri}}
                         style={{width: 150, height: 150}}
                     />
-                ) : ''}
+                ) : 'sdf'}
+
                 <input
                     type="file"
+                    multiple="multiple"
                     onChange={(event) => {
-                        setPhoto(event.target.files[0]);
+                        setPhotoss(event.target.files);
                     }}
                 />
                 <Button title={"העלאה"} onPress={() => uploadFile()}></Button>
