a069465179bdce942c48055a943654a6bef93538
Shlomi Yahbes <99732661+syahbes@users.noreply.github.com>
2022-12-04 00:08:47 +0200

    del all

--- src/api/uploadExcelFile/uploadExcelFileApi.js
+++
@@ -1,7 +0,0 @@
-async function uploadToServer() {
-    await Axios.post("https://upload-service-url", data, {
-        headers: {
-            "Content-Type": "multipart/form-data",
-        },
-    });
-}
