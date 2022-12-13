99c273c65782d87a990d2f85e674eb275cb0660a
itzikd1 <itzikd1@gmail.com>
2022-12-13 18:55:20 +0200

    api calls work perfectly

--- src/hooks/ApiCalls/authentication_provider.js
+++ src/hooks/ApiCalls/authentication_provider.js
@@ -2,13 +2,14 @@ import axios from "axios";
 import {useEffect, useState} from "react";
 
 export function getTokenAccess() {
-    // todo find a way to fix this so i can use it in get/post/put/delete --> probably use async somehow
+    // todo in the future move this to cookies
+    // todo in the future change the body so i get it as input from the user
+    // todo use the refresh token in the future for re login
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
     const headers = {headers: {'Content-Type': 'application/json'}};
 
-    //todo in the future change this so we get url and username from function getTokenAccess(url,body)
     const url = 'http://ec2-3-15-215-70.us-east-2.compute.amazonaws.com:8000/api/token/'
     const userLoginBody = {email: 'hameorer1@com.com', password: 'itizk12345'}
 
@@ -27,8 +28,7 @@ export function getTokenAccess() {
             });
     }, [url]);
 
-    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjcwODc3NjY1LCJpYXQiOjE2NzA4NzQwNjUsImp0aSI6IjQ2ODQ5ZmExM2YwNzRhODk5YjdiZGE1Nzc0NTkzZjEzIiwidXNlcl9pZCI6IjFmMDBjMjdkLTM3YWUtNGYzMC1iZmMxLWY2Nzk4MTYyN2Y3OSJ9.-VpemU2WQziG_Fz6QVFivr3nhgLvfxMsOGoR9rXhbT4'
-    return token
+    return {data, loading, error}
 }
 
 
--- src/hooks/ApiCalls/useFetchGet.js
+++ src/hooks/ApiCalls/useFetchGet.js
@@ -3,28 +3,29 @@ import {useEffect, useState} from "react";
 import {getTokenAccess} from "./authentication_provider";
 
 export default function UseFetchGet(url) {
-    const token = getTokenAccess()
+    const token = getTokenAccess().data
 
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
-    const headers = {headers: {'Content-Type': 'application/json', 'Authorization': `Bearer ${token}`}};
 
     useEffect(() => {
-        setLoading(true);
-        axios
-            .get(url, headers)
-            .then((response) => {
-                setData(response.data);
-            })
-            .catch((err) => {
-                setError(err);
-            })
-            .finally(() => {
-                setLoading(false);
-            });
-
-    }, []);
+        if(token) {
+            setLoading(true);
+            const headers = {headers: {'Content-Type': 'application/json', 'Authorization': `Bearer ${token}`}};
+            axios
+                .get(url, headers)
+                .then((response) => {
+                    setData(response.data);
+                })
+                .catch((err) => {
+                    setError(err);
+                })
+                .finally(() => {
+                    setLoading(false);
+                });
+        }
+    }, [token]);
 
     return {data, loading, error};
 }
