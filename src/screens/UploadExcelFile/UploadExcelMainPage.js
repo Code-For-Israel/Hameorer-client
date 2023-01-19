25143210a2d214577ee73975e03d33e68446ba1d
itzikd1 <itzikd1@gmail.com>
2023-01-19 21:47:34 +0200

    doc picker works

--- src/screens/UploadExcelFile/UploadExcelMainPage.js
+++ src/screens/UploadExcelFile/UploadExcelMainPage.js
@@ -6,22 +6,18 @@ import UseFetchPost from "../../hooks/ApiCalls/useFetchPost";
 import getSiteUrl from "../../utils/getSiteUrl";
 
 export function UploadExcelMainPage({setParsedData, parsedData}) {
-    const url =
-        getSiteUrl() + "v1/authentication/group/";
+    const url = getSiteUrl() + "v1/authentication/group/";
     const uploadFileConst = UseFetchPost(url, parsedData);
 
     const [fileUpload, setFileUpload] = useState(null);
 
     useEffect(() => {
-        if (uploadFileConst && uploadFileConst.error)
-            console.log("error", uploadFileConst);
+        if (uploadFileConst && uploadFileConst.error) console.log("error", uploadFileConst);
     }, [uploadFileConst]);
 
-    const FILE_TYPE_ACCEPTED =
-        ".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel";
+    const FILE_TYPE_ACCEPTED = ".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel";
     const showFileDetails = () => {
-        if (fileUpload !== null) return <Text>File to upload: {fileUpload.name}</Text>;
-        else return <Text></Text>;
+        if (fileUpload !== null) return <Text>File to upload: {fileUpload.name}</Text>; else return <Text></Text>;
     };
 
     async function uploadFile() {
@@ -39,70 +35,43 @@ export function UploadExcelMainPage({setParsedData, parsedData}) {
 
         //create the body of the post request
         const userGroupBody = {
-            group_name: fileUpload.name,
-            users: excelParsedToJsonData,
+            group_name: fileUpload.name, users: excelParsedToJsonData,
         };
 
         setParsedData(userGroupBody);
     }
 
-    return (
-        <>
-            {
-                <View style={{alignItems: "center", justifyContent: "center"}}>
-                    <Text>עמוד העלאה</Text>
-                    <Text>cant use input tag with react native - need to replace it</Text>
-                    <input
-                        type="file"
-                        accept={FILE_TYPE_ACCEPTED}
-                        onChange={(event) => {
-                            setFileUpload(event.target.files[0]);
-                        }}
-                    />
-                    <Button title={"העלאה"} onPress={() => uploadFile()}></Button>
-                    {showFileDetails()}
-                    <>
-                        {parsedData ? (
-                            <Text>File parsed - watch console log to see if uploaded</Text>
-                        ) : (
-                            ""
-                        )}
-                        {parsedData && parsedData.users && parsedData.users.length >= 1 ? (
-                            <>
-                                <Text>Items from excel that will be uploaded</Text>
-                                {parsedData.users.map((user) => {
-                                    return (
-                                        <Text key={user.email}>
-                                            {user.firstname +
-                                                " - " +
-                                                user.lastname +
-                                                " - " +
-                                                user.email}
-                                        </Text>
-                                    );
-                                })}
-                                {uploadFileConst && uploadFileConst.error ? (
-                                    <>
-                                        <Text>{uploadFileConst.error.code}</Text>
-                                        <Text>{uploadFileConst.error.message}</Text>
-                                    </>
-                                ) : (
-                                    <Text></Text>
-                                )}
-                                {uploadFileConst && uploadFileConst.data ? (
-                                    <>
-                                        <Text>Data uploaded successfully!</Text>
-                                    </>
-                                ) : (
-                                    <Text></Text>
-                                )}
-                            </>
-                        ) : (
-                            <Text></Text>
-                        )}
-                    </>
-                </View>
-            }
-        </>
-    );
+    return (<>
+            {<View style={{alignItems: "center", justifyContent: "center"}}>
+                <Text>עמוד העלאה</Text>
+                <Text>cant use input tag with react native - need to replace it</Text>
+                <input
+                    type="file"
+                    accept={FILE_TYPE_ACCEPTED}
+                    onChange={(event) => {
+                        setFileUpload(event.target.files[0]);
+                    }}
+                />
+                <Button title={"העלאה"} onPress={() => uploadFile()}></Button>
+                {showFileDetails()}
+                <>
+                    {parsedData ? (<Text>File parsed - watch console log to see if uploaded</Text>) : ("")}
+                    {parsedData && parsedData.users && parsedData.users.length >= 1 ? (<>
+                            <Text>Items from excel that will be uploaded</Text>
+                            {parsedData.users.map((user) => {
+                                return (<Text key={user.email}>
+                                        {user.firstname + " - " + user.lastname + " - " + user.email}
+                                    </Text>);
+                            })}
+                            {uploadFileConst && uploadFileConst.error ? (<>
+                                    <Text>{uploadFileConst.error.code}</Text>
+                                    <Text>{uploadFileConst.error.message}</Text>
+                                </>) : (<Text></Text>)}
+                            {uploadFileConst && uploadFileConst.data ? (<>
+                                    <Text>Data uploaded successfully!</Text>
+                                </>) : (<Text></Text>)}
+                        </>) : (<Text></Text>)}
+                </>
+            </View>}
+        </>);
 }
