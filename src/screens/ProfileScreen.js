90ad3e611caefada1d47be488cb9fa8a3d1c6b4f
Shlomi Yahbes <syahbes@gmail.com>
2023-01-20 09:23:23 +0200

    fixed the plus button issue

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,32 +1,34 @@
-import {Button, StyleSheet, Text, View} from "react-native";
+import { Button, StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import UseFetchGet from "../hooks/ApiCalls/useFetchGet";
-import {useState} from "react";
+import { useState } from "react";
 import getSiteUrl from "../utils/getSiteUrl";
 import BottomSheet from "../components/BottomSheet";
 import BottomMenuContent from "../components/BottomMenuContent";
-import {MaterialCommunityIcons} from "@expo/vector-icons";
-import {TouchableOpacity} from "react-native-web";
-import {UploadExcelFile} from "./UploadExcelFile/UploadExcelFile";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { UploadExcelFile } from "./UploadExcelFile/UploadExcelFile";
 
 export default function ProfileScreen() {
-    const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
-    const handlePress = () => {
-        setIsModalVisible(true);
-    };
-    const onModalClose = () => {
-        setIsModalVisible(false);
-    };
+  const handlePress = () => {
+    setIsModalVisible(true);
+  };
+  const onModalClose = () => {
+    setIsModalVisible(false);
+  };
 
-    const {data, loading} = UseFetchGet(getSiteUrl() + "v1/authentication/user/");
+  const { data, loading } = UseFetchGet(
+    getSiteUrl() + "v1/authentication/user/"
+  );
 
-    const [parsedData, setParsedData] = useState(null);
+  const [parsedData, setParsedData] = useState(null);
 
-    // enable this for getting the login
-    // const x = getTokenAccess()
+  // enable this for getting the login
+  // const x = getTokenAccess()
 
-    return (<View style={styles.container}>
-        <TouchableOpacity onPress={handlePress} style={{
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={handlePress} style={{
            position: "absolute", bottom: 20, right: 20,
         }}>
            <View
@@ -51,27 +53,30 @@ export default function ProfileScreen() {
            </View>
         </TouchableOpacity> 
 
-        <Text>Profile Screen</Text>
+      <Text>Profile Screen</Text>
 
-        <UploadExcelFile parsedData={parsedData} setParsedData={setParsedData}></UploadExcelFile>
+      <UploadExcelFile parsedData={parsedData} setParsedData={setParsedData} />
 
-        <Text>
-            {loading ? "Loading data ........." : "Data Loaded!"}
-        </Text>
+      <Text>
+        {loading ? "Loading data ........." : "Data Loaded!"}
+      </Text>
 
-        <Button
-            title={"click to pull groups and console log them"}
-            onPress={() => console.log(data)}
-        />
+      <Button
+        title={"click to pull groups and console log them"}
+        onPress={() => console.log(data)}
+      />
 
-        <BottomSheet isVisible={isModalVisible} onClose={onModalClose}>
-            <BottomMenuContent onClose={onModalClose}/>
-        </BottomSheet>
-    </View>);
+      <BottomSheet isVisible={isModalVisible} onClose={onModalClose}>
+        <BottomMenuContent onClose={onModalClose} />
+      </BottomSheet>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1, justifyContent: "center", alignItems: "center"
-    }
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
 });
