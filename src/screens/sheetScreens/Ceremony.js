5709776c0dfdf50f4fa3adbf0e79c3506b644c92
itzikd1 <itzikd1@gmail.com>
2023-01-20 18:27:24 +0200

    missing stuff

--- src/screens/sheetScreens/Ceremony.js
+++ src/screens/sheetScreens/Ceremony.js
@@ -1,82 +1,94 @@
-import {Text, View} from 'react-native'
-import React from 'react'
+import {Button, StyleSheet, Text, TextInput, View} from "react-native";
+import React, {useEffect, useState} from "react";
+
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {useDispatch, useSelector} from "react-redux";
+import {loginThunk, refreshAccess, selectAccess, selectRefresh, setAccess, setLoading,} from "../../redux/userSlice"; //note the path
+import {getStories} from "../../redux/dataSlice";
 
 const Ceremony = () => {
-  const [email, setEmail] = useState("hameorer2@com.com");
-  const [password, setPassword] = useState("shlomi12345");
+    const [email, setEmail] = useState("hameorer2@com.com");
+    const [password, setPassword] = useState("shlomi12345");
 
-  const dispatch = useDispatch();
-  const refresh = useSelector(selectRefresh);
-  const access = useSelector(selectAccess);
+    const dispatch = useDispatch();
+    const refresh = useSelector(selectRefresh);
+    const access = useSelector(selectAccess);
 
     const handleLogin = () => {
-    if (email && password) {
-      dispatch(loginThunk({ email, password }));
-    }
-  };
-  const refreshToken = () => {
-    dispatch(refreshAccess(refresh));
-  };
-  const getData = () => {
-    dispatch(getStories(access));
-  };
-
-  useEffect(() => {
-    const getLocal = async () => {
-      try {
-        const value = await AsyncStorage.getItem("@storage_Key");
-        if (value !== null) {
-          dispatch(getStories(value));
-          dispatch(setAccess(value));
-          dispatch(setLoading(false));
-        } else {
-          throw "There is No Token in Local Storage";
+        if (email && password) {
+            dispatch(loginThunk({email, password}));
         }
-      } catch (e) {
-        console.log(e);
-      }
     };
-    getLocal();
-  }, []);
+    const refreshToken = () => {
+        dispatch(refreshAccess(refresh));
+    };
+    const getData = () => {
+        dispatch(getStories(access));
+    };
 
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Redux</Text>
-      <TextInput
-        placeholder="email"
-        style={styles.input}
-        onChangeText={setEmail}
-        value={email}
-      />
-      <TextInput
-        placeholder="password"
-        style={styles.input}
-        onChangeText={setPassword}
-        value={password}
-      />
+    useEffect(() => {
+        const getLocal = async () => {
+            try {
+                const value = await AsyncStorage.getItem("@storage_Key");
+                if (value !== null) {
+                    dispatch(getStories(value));
+                    dispatch(setAccess(value));
+                    dispatch(setLoading(false));
+                } else {
+                    throw "There is No Token in Local Storage";
+                }
+            } catch (e) {
+                console.log(e);
+            }
+        };
+        getLocal();
+    }, []);
 
-      <Button onPress={handleLogin} title="Login" accessibilityLabel="Log in" />
-      <Text>---</Text>
-      <Button
-        onPress={refreshToken}
-        title="Refresh"
-        accessibilityLabel="Refresh"
-      />
-      <Text>---</Text>
-      <Button
-        onPress={getData}
-        title="Get data from backend"
-        accessibilityLabel="Get Data"
-      />
-      <Text>---</Text>
-      <Text>
-        The current state of the user is :
-        {access ? "logged in" : "not logged in"}
-      </Text>
-    
+    return (
+        <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+            <Text>Redux</Text>
+            <TextInput
+                placeholder="email"
+                style={styles.input}
+                onChangeText={setEmail}
+                value={email}
+            />
+            <TextInput
+                placeholder="password"
+                style={styles.input}
+                onChangeText={setPassword}
+                value={password}
+            />
 
-    </View>
-  );
+            <Button onPress={handleLogin} title="Login" accessibilityLabel="Log in"/>
+            <Text>---</Text>
+            <Button
+                onPress={refreshToken}
+                title="Refresh"
+                accessibilityLabel="Refresh"
+            />
+            <Text>---</Text>
+            <Button
+                onPress={getData}
+                title="Get data from backend"
+                accessibilityLabel="Get Data"
+            />
+            <Text>---</Text>
+            <Text>
+                The current state of the user is :
+                {access ? "logged in" : "not logged in"}
+            </Text>
+
+
+        </View>
+    );
 };
 
-export default Ceremony
+const styles = StyleSheet.create({
+    input: {
+        backgroundColor: "#fff",
+        marginBottom: 10,
+    },
+});
+
+export default Ceremony;
