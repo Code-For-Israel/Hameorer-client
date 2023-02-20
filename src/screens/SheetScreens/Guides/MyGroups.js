c11be0119c542b9bc6509770c06ab75a9b7e6fd4
itzikd1 <itzikd1@gmail.com>
2023-02-21 00:58:25 +0200

    working on guide page

--- src/screens/SheetScreens/Guides/MyGroups.js
+++ src/screens/SheetScreens/Guides/MyGroups.js
@@ -1,21 +1,44 @@
 import {ScrollView, StyleSheet, Text, View,} from "react-native";
 import React from "react";
-import {Provider} from "react-native-paper";
+import {IconButton, MD3Colors, Provider} from "react-native-paper";
 import {selectVisable,} from "../../../redux/dataSlice";
 import {useDispatch, useSelector} from "react-redux";
 import {selectAccess} from "../../../redux/userSlice";
-import GuideCard from "../../../components/GuideCard";
 import {styles} from "../../../styles/PagesStyle";
-
-const PlaceholderImage = require("../../../../assets/fallbackImage.png");
-
+import HorizonteScrollCards from "./HorizelScrollCards";
+import Icon from '@mdi/react';
+import {mdiAutoFix, mdiCheck, mdiClockTimeFiveOutline} from '@mdi/js';
 
 const MyGroup = () => {
-    let x = require("./mock_data.json")
-    console.log(x)
+    let mock_data = require("./mock_data.json")
+    console.log(mock_data)
     const dispatch = useDispatch();
     const visible = useSelector(selectVisable);
     const access = useSelector(selectAccess);
+    let pending = []
+    let review = []
+    let done = []
+
+    mock_data && mock_data.users.map((user) => {
+        let story
+        if (user && user.stories.length > 0) {
+            story = user.stories[0];
+            let storyObj = {
+                fullName: user.firstname + ' ' + user.lastname,
+                subject: story.subject.subject,
+                location: story.body.qoute_location,
+                dateBirth: 1902,
+                dateDeath: 1954
+            }
+            console.log(storyObj)
+            if (story.status === 'pending')
+                pending.push(storyObj)
+            if (story.status === 'review')
+                review.push(storyObj)
+            if (story.status === 'done')
+                done.push(storyObj)
+        }
+    })
 
     return (
         <Provider>
@@ -23,22 +46,27 @@ const MyGroup = () => {
                 <View style={stylesIn.HeadSection}>
                     <Text>הקבוצה שלי</Text>
                 </View>
-                <View style={stylesIn.HeadSection}>
-                    <Text>ממתין למשוב</Text>
-                    <GuideCard subject={'מרד גטו ורשה'} country={'הונגריה'} studentName={'יוני בלוך'} yearDeath={1955} yearBorn={1948}
-                               onPress={() => console.log("ok")}></GuideCard>
-                    <GuideCard subject={'מרד גטו ורשה'} country={'הונגריה'} studentName={'יוני בלוך'} yearDeath={1955} yearBorn={1948}
-                               onPress={() => console.log("ok")}></GuideCard>
-                    <GuideCard subject={'מרד גטו ורשה'} country={'הונגריה'} studentName={'יוני בלוך'} yearDeath={1955} yearBorn={1948}
-                               onPress={() => console.log("ok")}></GuideCard>
-                    <GuideCard subject={'מרד גטו ורשה'} country={'הונגריה'} studentName={'יוני בלוך'} yearDeath={1955} yearBorn={1948}
-                               onPress={() => console.log("ok")}></GuideCard>
+                <View>
+                    <View style={{flexDirection: 'row', alignSelf: 'flex-end'}}>
+                        <Text>ממתין למשוב ({pending.length})</Text>
+                        <Icon path={mdiClockTimeFiveOutline} size={1} />
+                    </View>
+                    <HorizonteScrollCards list={pending}></HorizonteScrollCards>
                 </View>
-                <View style={stylesIn.HeadSection}>
-                    <Text>הוחזר מתיקונים</Text>
+                <View>
+                    <View style={{flexDirection: 'row', alignSelf: 'flex-end'}}>
+                        <Text>הוחזר מתיקונים ({review.length})</Text>
+                        <Icon path={mdiAutoFix} size={1} />
+
+                    </View>
+                    <HorizonteScrollCards list={review}></HorizonteScrollCards>
                 </View>
-                <View style={stylesIn.HeadSection}>
-                    <Text>אושר</Text>
+                <View>
+                    <View style={{flexDirection: 'row', alignSelf: 'flex-end'}}>
+                        <Text>אושר({done.length})</Text>
+                        <Icon path={mdiCheck} size={1} />
+                    </View>
+                    <HorizonteScrollCards list={done}></HorizonteScrollCards>
                 </View>
             </ScrollView>
         </Provider>
@@ -47,137 +75,11 @@ const MyGroup = () => {
 
 export default MyGroup;
 
-const containerStyle = {
-    backgroundColor: "#fff",
-    padding: 10,
-    width: 300,
-    alignSelf: "center",
-    borderRadius: 15,
-};
-
 const stylesIn = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-    },
-    footerContainer: {
-        marginTop: 10,
-        flexDirection: "row",
-        justifyContent: "space-between",
-        paddingHorizontal: 20,
-    },
-    checkboxContainer: {
-        marginTop: 10,
-        flexDirection: "row",
-        justifyContent: "center",
-        paddingHorizontal: 20,
-    },
-    nextText: {
-        opacity: 0,
-    },
-    headText: {
-        fontSize: 16,
-    },
-    ProgressBarContainer: {
-        marginTop: 5,
-        width: "90%",
-        display: "flex",
-        marginLeft: "5%",
-    },
-    ProgressBarStyle: {
-        height: 8,
-        backgroundColor: "#ADBCF2",
-    },
     HeadSection: {
         padding: 5,
-        flexDirection: "column",
+        flexDirection: "row",
         alignSelf: "flex-end",
         justifyContent: "space-between",
     },
-    plusContainer: {
-        fontSize: 24,
-        backgroundColor: "#FCBF49",
-        width: 67,
-        height: 67,
-        borderRadius: 67,
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        // shadow
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-
-        elevation: 5,
-    },
-    detailsContainer: {
-        display: "flex",
-        flexDirection: "column",
-    },
-    h1: {
-        fontSize: 24,
-        color: "#2B3F66",
-        fontWeight: "bold",
-        marginBottom: 15,
-    },
-    textBody: {
-        fontSize: 16,
-        marginBottom: 5,
-    },
-    textSubTitle: {
-        fontSize: 16,
-        fontWeight: "bold",
-        marginBottom: 5,
-    },
-    TextInputContainer: {
-        width: "100%",
-        paddingHorizontal: 15,
-    },
-    TextCheckbox: {
-        alignSelf: "center",
-    },
-    input: {
-        marginBottom: 10,
-        padding: 10,
-        backgroundColor: "#f5f5f5",
-        borderRadius: 10,
-        fontSize: 18,
-        textAlign: "right",
-    },
-    inputBig: {
-        height: 280,
-        textAlignVertical: "top",
-    },
-    inputSound: {
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-    },
-    send: {
-        width: 100,
-        marginLeft: 15,
-        marginTop: 5,
-    },
-    ImageContainer: {
-        // flex: 1,
-        // padding: 1,
-        borderRadius: 65,
-        shadowColor: "#000000",
-        shadowOffset: {
-            width: 0,
-            height: 3,
-        },
-        shadowOpacity: 0.17,
-        shadowRadius: 3.05,
-        elevation: 4,
-    },
-    greyText: {
-        alignSelf: "center", color: "#8B8787", marginBottom: 10
-    }
 });
