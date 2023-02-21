0204792839ec38e740b6c4ea5fa49391f390451c
yuvalste13 <yuvalste13@gmail.com>
2023-02-21 20:36:23 +0200

    guide card changes

---
+++ src/components/GuideCard.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { Image, TouchableOpacity, View } from "react-native";
+import { Card, Text } from "react-native-paper";
+import { styles } from "../styles/PagesStyle";
+import PlaceholderImage from "../../assets/fallbackImage.png";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+
+const B = (props) => (
+  <Text style={{ fontWeight: "bold" }}>{props.children}</Text>
+);
+
+const guideCard = ({
+  studentName,
+  subject,
+  country,
+  yearBorn,
+  yearDeath,
+  onPress,
+}) => (
+  <>
+    <View>
+      <Card style={styles.cardComponent}>
+        <Card.Content>
+          <View style={{ height: 26 }}>
+            <View style={styles.cardComponentTopPart}>
+              <View>
+                <MaterialIcons name="mail" color="#fff" size={15} />
+              </View>
+              <View>
+                <Text style={styles.cardComponentText}>{studentName}</Text>
+              </View>
+            </View>
+          </View>
+
+          <View style={styles.cardComponentInsideTextArea}>
+            <View style={styles.cardComponentCInsideContainer}>
+              <Image
+                source={PlaceholderImage}
+                style={styles.cardComponentImage}
+              ></Image>
+            </View>
+            <View style={styles.cardComponentTextContainer}>
+              <Text style={styles.cardComponentTextSize}>
+                <B>נושא:</B> {subject}
+              </Text>
+              <Text style={styles.cardComponentTextSize}>
+                <B>מדינה:</B> {country}
+              </Text>
+              <Text style={styles.cardComponentTextSize}>
+                {yearBorn} <B>:תאריך לידה</B>
+              </Text>
+              <Text style={styles.cardComponentTextSize}>
+                {yearDeath} <B>:תאריך פטירה</B>
+              </Text>
+              <TouchableOpacity
+                style={styles.cardComponentButton}
+                onPress={onPress}
+              >
+                <Text style={styles.cardComponentText}>{"לציטוט המלא"}</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Card.Content>
+      </Card>
+    </View>
+  </>
+);
+
+export default guideCard;
---
+++ src/screens/HorizelScrollCards.js
@@ -0,0 +1,39 @@
+import { ScrollView, StyleSheet, View } from "react-native";
+import GuideCard from "../components/GuideCard";
+
+const HorizonteScrollCards = ({ list }) => {
+  return (
+    <ScrollView horizontal={true}>
+      {
+        <>
+          <View style={stylesIn.HeadSection}>
+            {list.map((card, key) => {
+              return (
+                <GuideCard
+                  key={key}
+                  subject={card.subject}
+                  country={card.location}
+                  studentName={card.fullName}
+                  yearDeath={card.dateDeath}
+                  yearBorn={card.birthDate}
+                  onPress={() => console.log("ok")}
+                ></GuideCard>
+              );
+            })}
+          </View>
+        </>
+      }
+    </ScrollView>
+  );
+};
+
+export default HorizonteScrollCards;
+
+const stylesIn = StyleSheet.create({
+  HeadSection: {
+    padding: 5,
+    flexDirection: "row",
+    alignSelf: "normal",
+    justifyContent: "space-between",
+  },
+});
