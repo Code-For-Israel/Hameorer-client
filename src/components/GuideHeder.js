2d5e73527a951e722678de54697f1d7f1dd94afa
yuvalste13 <yuvalste13@gmail.com>
2023-02-21 19:51:54 +0200

    icona added

--- src/components/GuideHeder.js
+++ src/components/GuideHeder.js
@@ -1,60 +1,99 @@
-import {Image, StyleSheet, Text, View} from "react-native";
-
+import { Image, StyleSheet, Text, View } from "react-native";
 import React from "react";
 
 const PlaceholderImage = require("../../assets/fallbackImage.png");
+const NotificationIcon = require("../../assets/notificationIcon.png");
 
 const GuideHeader = () => {
-    return (<View style={styles.container}>
-        <View style={styles.header}>
-            <Text style={styles.h1}>משלחת רוגוזין 2023</Text>
-            <Text style={styles.h2}>הקבוצה של רפי</Text>
-        </View>
-        <View></View>
-        <View style={styles.circle}>
-            <Image source={PlaceholderImage} style={styles.image}></Image>
-        </View>
-        <View style={styles.name}>
-            <Text style={styles.h3}>יאנוש קורצק</Text>
-        </View>
-    </View>);
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.h1}>משלחת רוגוזין 2023</Text>
+        <Text style={styles.h2}>הקבוצה של רפי</Text>
+      </View>
+      <View style={styles.notificationIconContainer}>
+        <Image source={NotificationIcon} style={styles.notificationIcon} />
+      </View>
+      <View style={styles.circle}>
+        <Image source={PlaceholderImage} style={styles.image} />
+      </View>
+      <View style={styles.name}>
+        <Text style={styles.h3}>יאנוש קורצק</Text>
+      </View>
+    </View>
+  );
 };
 
 export default GuideHeader;
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: "#072F5F", borderBottomLeftRadius: 50, flexDirection: "row", height: 150,
-    }, header: {
-        flexDirection: "column",
-        width: "100%",
-        height: 100,
-        position: "relative",
-        top: 0,
-        left: 0,
-        justifyContent: "center",
-        alignItems: "center",
-    }, h1: {
-        color: "#fff", fontSize: 40, fontFamily: "arial", margin: 20,
-    }, h2: {
-        color: "#fff", fontFamily: "arial", fontSize: 20,
-    }, h3: {
-        color: "#fff", fontFamily: "arial", fontSize: 14,
-    }, name: {
-        height: 75, top: 90, left: 140, levation: 10, position: "absolute",
-    }, img: {
-        borderRadius: 50, alignContent: "center",
-    }, circle: {
-        height: 50,
-        flexDirection: "column",
-        width: 50,
-        borderRadius: 50,
-        position: "absolute",
-        top: 30,
-        left: 150,
-        elevation: 10,
-        backgroundColor: "white",
-    }, image: {
-        width: 130, height: 130, borderRadius: 65, resizeMode: "contain",
-    },
+  container: {
+    backgroundColor: "#072F5F",
+    borderBottomLeftRadius: 50,
+    flexDirection: "row",
+    height: 150,
+  },
+  header: {
+    flexDirection: "column",
+    width: "100%",
+    height: 100,
+    position: "relative",
+    top: 0,
+    left: 0,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  h1: {
+    color: "#fff",
+    fontSize: 40,
+    fontFamily: "arial",
+    margin: 20,
+  },
+  h2: {
+    color: "#fff",
+    fontFamily: "arial",
+    fontSize: 20,
+  },
+  h3: {
+    color: "#fff",
+    fontFamily: "arial",
+    fontSize: 14,
+  },
+  name: {
+    height: 75,
+    top: 90,
+    left: 140,
+    levation: 10,
+    position: "absolute",
+  },
+  img: {
+    borderRadius: 50,
+    alignContent: "center",
+  },
+  circle: {
+    height: 50,
+    flexDirection: "column",
+    width: 50,
+    borderRadius: 50,
+    position: "absolute",
+    top: 30,
+    left: 150,
+    elevation: 10,
+    backgroundColor: "white",
+  },
+  image: {
+    width: 50,
+    height: 50,
+    resizeMode: "contain",
+  },
+  notificationIconContainer: {
+    position: "absolute",
+    top: 10,
+    right: 10,
+  },
+  notificationIcon: {
+    width: 50,
+    height: 50,
+    tintColor: "white",
+  },
 });
--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,167 +1,180 @@
-import {StyleSheet, Text, TouchableOpacity, View,} from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  SafeAreaView,
+  ScrollView,
+} from "react-native";
 // import UseFetchGet from "../hooks/ApiCalls/useFetchGet";
-import {useState} from "react";
+import { useState } from "react";
+
 // import getSiteUrl from "../utils/getSiteUrl";
 import BottomSheet from "../components/BottomSheet";
 import BottomMenuContent from "../components/BottomMenuContent";
-import {MaterialCommunityIcons} from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import ImageViewer from "../components/ImageViewer";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import {useDispatch} from "react-redux";
-import {setAccess, setRefresh} from "../redux/userSlice";
+import { useDispatch } from "react-redux";
+import { setAccess, setRefresh } from "../redux/userSlice";
+
 import GuideHeader from "../components/GuideHeder";
 import PrevButton from "../components/NextButton";
 
 export default function ProfileScreen() {
-    const [isModalVisible, setIsModalVisible] = useState(false);
-    const dispatch = useDispatch();
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const dispatch = useDispatch();
 
-    const PlaceholderImage = require("../../assets/fallbackImage.png");
-    const handlePress = () => {
-        setIsModalVisible(true);
-    };
-    const onModalClose = () => {
-        setIsModalVisible(false);
-    };
+  const PlaceholderImage = require("../../assets/fall.jpeg");
+  const handlePress = () => {
+    setIsModalVisible(true);
+  };
+  const onModalClose = () => {
+    setIsModalVisible(false);
+  };
 
-    // const {data, loading} = UseFetchGet(
-    //     getSiteUrl() + "v1/authentication/user/"
-    // );
-    const data = "";
-    const loading = "";
-    const [parsedData, setParsedData] = useState(null);
+  // const {data, loading} = UseFetchGet(
+  //     getSiteUrl() + "v1/authentication/user/"
+  // );
 
-    // enable this for getting the login
-    // const x = getTokenAccess()
+  const data = "";
+  const loading = "";
+  const [parsedData, setParsedData] = useState(null);
 
-    const logout = async () => {
-        //first clear the local storage
-        try {
-            await AsyncStorage.removeItem("refreshToken");
-        } catch (e) {
-            // remove error
-        }
-        //then update the redux store
-        dispatch(setAccess(""));
-        dispatch(setRefresh(""));
-    };
+  // enable this for getting the login
+  // const x = getTokenAccess()
 
-    return (
-        <View style={styles.container}>
-            <GuideHeader
-                style={{
-                    width: "100%",
-                    flexDirection: "row",
-                    justifyContent: "flex-end",
-                }}
-            />
-            <TouchableOpacity
-                onPress={handlePress}
-                style={{
-                    position: "absolute",
-                    bottom: 20,
-                    right: 20,
-                }}
-            >
-                <View
-                    style={{
-                        justifyContent: "center",
-                        alignItems: "center",
-                        width: 50,
-                        height: 50,
-                        borderRadius: 50,
-                        backgroundColor: "#FCBF49",
-                        color: "#fff",
-                        shadowColor: "#000",
-                        shadowOffset: {
-                            width: 0,
-                            height: 2,
-                        },
-                        shadowOpacity: 0.25,
-                        shadowRadius: 3.84,
-                        elevation: 5,
-                    }}
-                >
-                    <MaterialCommunityIcons name="plus" size={25} color={"#fff"}/>
-                </View>
-            </TouchableOpacity>
-            <ImageViewer
-                style={styles.image1}
-                placeholderImageSource={PlaceholderImage}
-            />
-            <View>
-                <Text style={styles.h1}>הודעות</Text>
-            </View>
-            <View style={{marginRight: 20}}>
-                <Text>מאת עמית-12:00</Text>
-                <Text>
-                    {" "}
-                    עוד יומיים ניפגש לסדנה שנייה בתהליך ההכנה נראה את הסרט ״סיפורו של נער
-                    ארטיליאי״ ונדבר מה הופך ילדתמים לחלק מחבורה גזענית
-                </Text>
-            </View>
+  const logout = async () => {
+    //first clear the local storage
+    try {
+      await AsyncStorage.removeItem("refreshToken");
+    } catch (e) {
+      // remove error
+    }
+    //then update the redux store
+    dispatch(setAccess(""));
+    dispatch(setRefresh(""));
+  };
 
-            <View style={{width: 200, marginLeft: 20, paddingBottom: 20}}>
-                <PrevButton title="לחץ לכל ההודעות"/>
-            </View>
+  return (
+    <View style={styles.container}>
+      <GuideHeader
+        style={{
+          width: "100%",
+          flexDirection: "row",
+          justifyContent: "flex-end",
+        }}
+      />
+      <View style={styles.image1}>
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+      <TouchableOpacity
+        onPress={handlePress}
+        style={{
+          position: "absolute",
+          bottom: 20,
+          right: 20,
+        }}
+      >
+        <View
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            width: 50,
+            height: 50,
+            borderRadius: 50,
+            backgroundColor: "#FCBF49",
+            color: "#fff",
+            shadowColor: "#000",
+            shadowOffset: {
+              width: 0,
+              height: 2,
+            },
+            shadowOpacity: 0.25,
+            shadowRadius: 3.84,
+            elevation: 5,
+          }}
+        >
+          <MaterialCommunityIcons name="plus" size={25} color={"#fff"} />
+        </View>
+      </TouchableOpacity>
 
-            <View>
-                <Text style={styles.h1}>סטטוס משימות</Text>
-            </View>
-            <View
-                style={{
-                    width: "100%",
-                    flexDirection: "row",
-                    justifyContent: "flex-end",
-                }}
-            ></View>
+      <View>
+        <Text style={styles.h1}>הודעות</Text>
+      </View>
+      <View style={{ marginRight: 20 }}>
+        <Text>מאת עמית-12:00</Text>
+        <Text>
+          {" "}
+          עוד יומיים ניפגש לסדנה שנייה בתהליך ההכנה נראה את הסרט ״סיפורו של נער
+          ארטיליאי״ ונדבר מה הופך ילדתמים לחלק מחבורה גזענית
+        </Text>
+      </View>
 
-            {/* <View style={{ width: 100, alignContent: "center", marginLeft: 20 }}>
+      <View style={{ width: 200, marginLeft: 20, paddingBottom: 20 }}>
+        <PrevButton title="לחץ לכל ההודעות" />
+      </View>
+
+      <View>
+        <Text style={styles.h1}>סטטוס משימות</Text>
+      </View>
+
+      <View
+        style={{
+          width: "100%",
+          flexDirection: "row",
+          justifyContent: "flex-end",
+        }}
+      ></View>
+
+      {/* <View style={{ width: 100, alignContent: "center", marginLeft: 20 }}>
         <PrevButton title="Log Out" onPress={logout} />
       </View> */}
 
-            {/* <UploadExcelFile parsedData={parsedData} setParsedData={setParsedData} /> */}
-            {/* <Text>{loading ? "Loading data ........." : "Data Loaded!"}</Text>
+      {/* <UploadExcelFile parsedData={parsedData} setParsedData={setParsedData} /> */}
+      {/* <Text>{loading ? "Loading data ........." : "Data Loaded!"}</Text>
       <Button
         title={"click to pull groups and console log them"}
         onPress={() => console.log(data)}
       />
 */}
-            <BottomSheet isVisible={isModalVisible} onClose={onModalClose}>
-                <BottomMenuContent onClose={onModalClose}/>
-            </BottomSheet>
-        </View>
-    );
+      <BottomSheet isVisible={isModalVisible} onClose={onModalClose}>
+        <BottomMenuContent onClose={onModalClose} />
+      </BottomSheet>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        position: "relative",
-    },
-    image1: {
-        position: "absolute",
-        width: 100,
-        height: 100,
-        alignContent: "center",
-        marginLeft: 20,
-        resizeMode: "",
-    },
-    detailsContainer: {
-        display: "flex",
-        flexDirection: "column",
-    },
-    h1: {
-        fontSize: 24,
-        color: "#072F5F",
-        fontWeight: "bold",
-        marginBottom: 15,
-        marginTop: 15,
-    },
-    image: {
-        width: 130,
-        height: 130,
-        borderRadius: 65,
-        resizeMode: "contain",
-    },
+  container: {
+    backgroundColor: "white",
+    flex: 1,
+    position: "relative",
+  },
+  image1: {
+    position: "absolute",
+    width: 100,
+    height: 100,
+    alignContent: "center",
+    marginLeft: 20,
+    resizeMode: "",
+  },
+  detailsContainer: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  h1: {
+    fontSize: 24,
+    color: "#072F5F",
+    fontWeight: "bold",
+    marginBottom: 15,
+    marginTop: 15,
+    marginRight: 15,
+  },
+  image: {
+    width: 130,
+    height: 130,
+    borderRadius: 65,
+    resizeMode: "contain",
+  },
 });
