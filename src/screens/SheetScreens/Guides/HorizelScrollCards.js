a63b866c6756130fea65f98b79bdeb9fae88780c
itzikd1 <itzikd1@gmail.com>
2023-02-22 01:03:54 +0200

    working on guide page

--- src/screens/SheetScreens/Guides/HorizelScrollCards.js
+++ src/screens/SheetScreens/Guides/HorizelScrollCards.js
@@ -5,11 +5,11 @@ const HorizonteScrollCards = ({list}) => {
     return <ScrollView horizontal={true} style={{flexDirection: 'row-reverse'}}>{<>
         <View style={stylesIn.HeadSection}>
             {list && list.length > 0 && list.map((card, key) => {
-                return (<GuideCard key={key} subject={card.subject}
-                                   country={card.location}
-                                   studentName={card.fullName}
-                                   yearDeath={card.dateDeath}
-                                   yearBorn={card.dateBirth}
+                return (<GuideCard key={key} subject={card?.subject}
+                                   country={card?.location}
+                                   studentName={card?.fullName}
+                                   yearDeath={card?.dateDeath}
+                                   yearBorn={card?.dateBirth}
                                    onPress={() => console.log("ok")}></GuideCard>)
             })}
         </View>
--- src/screens/SheetScreens/Guides/MyGroups.js
+++ src/screens/SheetScreens/Guides/MyGroups.js
@@ -1,25 +1,35 @@
 import {ScrollView, StyleSheet, Text, View,} from "react-native";
-import React from "react";
-import {List, Provider} from "react-native-paper";
-import {selectVisable,} from "../../../redux/dataSlice";
-import {useDispatch, useSelector} from "react-redux";
-import {selectAccess} from "../../../redux/userSlice";
+import React, {useEffect, useState} from "react";
+import {Provider} from "react-native-paper";
 import {styles} from "../../../styles/PagesStyle";
 import HorizonteScrollCards from "./HorizelScrollCards";
 import Icon from '@mdi/react';
 import {mdiAutoFix, mdiCheck, mdiClockTimeFiveOutline} from '@mdi/js';
 
 const MyGroup = () => {
-    let mock_data = require("./mock_data.json")
-    console.log(mock_data)
-    const dispatch = useDispatch();
-    const visible = useSelector(selectVisable);
-    const access = useSelector(selectAccess);
+    const baseUrl = "http://3.140.113.123:8000/";
+    const access = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjc3MDIzMDMxLCJpYXQiOjE2NzcwMTk0MzEsImp0aSI6IjBlZGJmMWM5YWEwNTQ0ODk5YTE2MDBkNGM5MGEzMDE3IiwidXNlcl9pZCI6IjFmMDBjMjdkLTM3YWUtNGYzMC1iZmMxLWY2Nzk4MTYyN2Y3OSJ9.A8_OUxMnMq-n271YFdaGm4G64ti0LneFuwXnUB3yHxY"
+    const [isLoading, setLoading] = useState(true);
+    const [groupInfo, setGroupInfo] = useState([]);
+
+    useEffect(() => {
+        if (access) {
+            fetch(`${baseUrl}api/v1/authentication/groupinfo`, {
+                method: "GET", headers: {
+                    accept: "application/json", "Content-Type": "application/json", Authorization: `Bearer ${access}`,
+                },
+            }).then((response) => response.json())
+                .then((json) => setGroupInfo(json))
+                .catch((error) => console.error(error))
+                .finally(() => setLoading(false));
+        }
+    }, [access]);
+
     let pending = []
     let review = []
     let done = []
 
-    mock_data && mock_data.users.map((user) => {
+    groupInfo && groupInfo.users && groupInfo.users.map((user) => {
         let story
         if (user && user.stories.length > 0) {
             story = user.stories[0];
@@ -30,53 +40,40 @@ const MyGroup = () => {
                 dateBirth: 1902,
                 dateDeath: 1954
             }
-            console.log(storyObj)
-            if (story.status === 'pending')
-                pending.push(storyObj)
-            if (story.status === 'review')
-                review.push(storyObj)
             if (story.status === 'done')
                 done.push(storyObj)
+            if (story.status === 'review')
+                review.push(storyObj)
+            if (story.status === 'pending')
+                pending.push(storyObj)
         }
     })
 
     return (
         <Provider>
             <ScrollView style={styles.mainContainer}>
-                <View style={stylesIn.HeaderSection}>
-                    <View style={{width: "100%"}}>
-                        <List.Accordion
-                            style={{background: '#D9D9D9', height: 47, textAlignLast: 'right', borderRadius: 5}}
-                            title="הקבוצה שלי"
-                            left={props => <List.Icon {...props} icon="file-edit-outline"/>}>
-                            <List.Item title="חלק 1 שנפתח" style={{textAlignLast: 'right'}}/>
-                            <List.Item title="?חלק 2 שנפתח - מה שמים פה" style={{textAlignLast: 'right'}}/>
-                        </List.Accordion>
-                    </View>
-                </View>
-                <View style={{alignSelf: 'center'}}>
-                    <Text style={stylesIn.groupSubtitle}>ציטוט מונפש
-                        ({pending.length + review.length + done.length})</Text>
+                <View style={stylesIn.HeadSection}>
+                    <Text>הקבוצה שלי</Text>
                 </View>
                 <View>
-                    <View style={{flexDirection: 'row', alignSelf: 'flex-end', paddingTop: 5}}>
-                        <Text style={styles.cardComponentTextBlack}>ממתין למשוב ({pending.length})</Text>
-                        <Icon path={mdiClockTimeFiveOutline} size={1} style={{paddingTop: 5, paddingLeft: 2}}/>
+                    <View style={{flexDirection: 'row', alignSelf: 'flex-end'}}>
+                        <Text>ממתין למשוב ({pending.length})</Text>
+                        <Icon path={mdiClockTimeFiveOutline} size={1}/>
                     </View>
                     <HorizonteScrollCards list={pending}></HorizonteScrollCards>
                 </View>
                 <View>
                     <View style={{flexDirection: 'row', alignSelf: 'flex-end'}}>
-                        <Text style={styles.cardComponentTextBlack}>הוחזר מתיקונים ({review.length})</Text>
-                        <Icon path={mdiAutoFix} size={1} style={{paddingTop: 5, paddingLeft: 2}}/>
+                        <Text>הוחזר מתיקונים ({review.length})</Text>
+                        <Icon path={mdiAutoFix} size={1}/>
 
                     </View>
                     <HorizonteScrollCards list={review}></HorizonteScrollCards>
                 </View>
                 <View>
                     <View style={{flexDirection: 'row', alignSelf: 'flex-end'}}>
-                        <Text style={styles.cardComponentTextBlack}>אושר({done.length})</Text>
-                        <Icon path={mdiCheck} size={1} style={{paddingTop: 5, paddingLeft: 2}}/>
+                        <Text>אושר({done.length})</Text>
+                        <Icon path={mdiCheck} size={1}/>
                     </View>
                     <HorizonteScrollCards list={done}></HorizonteScrollCards>
                 </View>
