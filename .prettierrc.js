b213aedd8330cba333c0e26ca47c45700092fdad
itzikd1 <itzikd1@gmail.com>
2023-02-27 09:23:08 +0200

    eslint

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,4 +2,13 @@ module.exports = {
     singleQuote: true,
     trailingComma: 'all',
     printWidth: 100,
+    "arrowParens": "always",
+    "bracketSpacing": true,
+    "jsxBracketSameLine": false,
+    "jsxSingleQuote": false,
+    "quoteProps": "as-needed",
+    "semi": true,
+    "useTabs": false,
+    "tabWidth": 2,
 };
+
