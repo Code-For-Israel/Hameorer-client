6994b298ebd326a8d00eca21255501bce588c841
itzikd1 <itzikd1@gmail.com>
2023-04-11 22:34:28 +0300

    merge

--- src/screens/SheetScreens/Students/SelfGuide/Page1.js
+++ src/screens/SheetScreens/Students/SelfGuide/Page1.js
@@ -7,54 +7,36 @@ import {MaterialBottomScroll} from '../../../../components/materialBottomScroll/
 import {PageTop} from './PageTop';
 
 const Page1 = ({route, navigation}) => {
-    const selectedSub = route.params.subject;
-    const [open, setOpen] = useState(false);
-    const [value, setValue] = useState('one');
-    const [items, setItems] = useState([
-        {label: 'יאנוש קורצק', value: 'one'},
-        {label: 'כדורגל בשואה', value: 'two'},
-        {label: 'משפט אייכמן', value: 'three'},
-        {label: 'מרד גטו וארשה', value: 'four'},
-    ]);
-    const dateEvent = 1943;
+    const selectedSub = route.params;
+
+    const dateEvent = 1943
     const B = (props) => <Text>{props.children}</Text>;
 
     const [text, setText] = useState('');
 
-    return (
-        <SafeAreaView style={{flex: 1}}>
-            <View style={styles.pageContainer}>
-                {PageTop(
-                    setText,
-                    text,
-                    selectedSub,
-                    '1943',
-                    'פתיחה',
-                    'הסבירו על הנושא שבחרתם במילים שלכם ומדוע בחרתם בו?',
-                )}
+    return (<SafeAreaView style={{flex: 1}}>
+        <View style={styles.pageContainer}>
 
-                <MaterialBottomScroll></MaterialBottomScroll>
+            {PageTop(setText, text, selectedSub.subject, '1943', 'פתיחה', 'הסבירו על הנושא שבחרתם במילים שלכם ומדוע בחרתם בו?')}
 
-                <View style={styles.ButtonContainer}>
-                    <View style={{width: 100}}>
-                        <NextButton
-                            title="הבא"
-                            onPress={() => {
-                                navigation.navigate('Page2', {
-                                    textPage1: text,
-                                    selectedSub: selectedSub,
-                                });
-                            }}
-                        />
-                    </View>
+            <MaterialBottomScroll></MaterialBottomScroll>
 
-                    <View>
-                        <Text>שלב 1 מתוך 5</Text>
-                    </View>
+            <View style={styles.ButtonContainer}>
+                <View style={{width: 100}}>
+                    <NextButton
+                        title="הבא"
+                        onPress={() => {
+                                navigation.navigate('Page2', {textPage1: text, selectedSub: selectedSub});
+                        }}
+                    />
+                </View>
+
+                <View>
+                    <Text>שלב 1 מתוך 5</Text>
                 </View>
             </View>
-        </SafeAreaView>
-    );
+        </View>
+    </SafeAreaView>);
 };
 
 export default Page1;
