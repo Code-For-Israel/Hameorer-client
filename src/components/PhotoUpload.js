2370afbe2fe3b8b3e000bd161a6bcfe2d41b972a
itzikd1 <itzikd1@gmail.com>
2023-05-15 01:40:45 +0300

    work on photo upload

--- src/components/PhotoUpload.js
+++ src/components/PhotoUpload.js
@@ -1,8 +1,13 @@
 import React, {useState, useEffect} from 'react';
 import {Button, Image, StyleSheet, View} from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+import GetSiteUrl from "../utils/GetSiteUrl";
+import {useSelector} from "react-redux";
+import {selectAccess} from "../redux/userSlice";
 
 export default function PhotoUpload({imageList, setImageList}) {
+    const access = useSelector(selectAccess);
+
     useEffect(() => {
         console.log(imageList);
     }, [imageList]);
@@ -19,10 +24,26 @@ export default function PhotoUpload({imageList, setImageList}) {
             setImageList([...imageList, result.assets[0]]);
         }
     };
+    const upload = async () => {
+        let response;
+        console.log(imageList)
+        const bucket = 'hameorer-img';
+        const baseUrl = GetSiteUrl();
+        response = await fetch(`${baseUrl}v1/media/${bucket}/${'test.jpg'}/`, {
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${access}`,
+            },
+            body: imageList[0],
+        }).then((response) => response.json());
+        console.log(response)
+    }
 
     return (
         <View style={styles.container}>
             <Button title="בחר תמונות" onPress={pickImage} />
+
+            <Button title="שלח" onPress={upload} />
 
             <View style={styles.imageList}>
                 {imageList &&
