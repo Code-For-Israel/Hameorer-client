b8c81146c67450e2c3eefabe70a36493b4550ecc
itzikd1 <itzikd1@gmail.com>
2023-05-16 20:36:42 +0300

    upload photo works

--- src/components/PhotoUpload.js
+++ src/components/PhotoUpload.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import {Button, Image, StyleSheet, View} from 'react-native';
-import GetSiteUrl from "../utils/GetSiteUrl";
-import {useSelector} from "react-redux";
-import {selectAccess} from "../redux/userSlice";
-import * as DocumentPicker from "expo-document-picker";
+import GetSiteUrl from '../utils/GetSiteUrl';
+import {useSelector} from 'react-redux';
+import {selectAccess} from '../redux/userSlice';
+import * as DocumentPicker from 'expo-document-picker';
 
 export default function PhotoUpload({imageList, setImageList, respondsList, setRespondsList}) {
     const access = useSelector(selectAccess);
@@ -11,34 +11,36 @@ export default function PhotoUpload({imageList, setImageList, respondsList, setR
     const bucket = 'hameorer-img';
     const baseUrl = GetSiteUrl();
 
+    const clearPhotos = async () => {
+    setImageList([]);
+    setRespondsList([]);
+    };
+
     const pickImage = async () => {
         let result = await DocumentPicker.getDocumentAsync({type: ALLOWED_TYPES});
         if (result.type === 'success') {
-            if (ALLOWED_TYPES.includes(result.mimeType) === false)
+            if (ALLOWED_TYPES.includes(result.mimeType) === false) {
                 console.log('wrong type of file - only img');
-            else {
+            } else {
                 setImageList([...imageList, result]);
             }
         }
     };
 
     const upload = async () => {
+    setRespondsList([]);
         let formData = new FormData();
         if (imageList && imageList.length > 0) {
             const uploadPromises = imageList.map(async (img) => {
                 formData.append('type', img.mimeType);
                 formData.append('file', {
-                    uri: img.uri,
-                    name: img.name,
-                    type: img.mimeType,
+                    uri: img.uri, name: img.name, type: img.mimeType,
                 });
 
                 const response = await fetch(`${baseUrl}v1/media/${bucket}/${img.name}/`, {
-                    method: 'POST',
-                    headers: {
+                    method: 'POST', headers: {
                         Authorization: `Bearer ${access}`,
-                    },
-                    body: formData,
+                    }, body: formData,
                 });
 
                 return response.json();
@@ -47,43 +49,39 @@ export default function PhotoUpload({imageList, setImageList, respondsList, setR
             const responses = await Promise.all(uploadPromises);
 
             setRespondsList([...respondsList, ...responses]);
-            console.log('ok');
         }
     };
 
+    return (<View style={styles.container}>
+        <View style={styles.containerLong}>
+            <Button title="מחק תמונות" onPress={clearPhotos} color="#FF3B30"/>
 
-    return (
-        <View style={styles.container}>
-            <Button title="בחר תמונות" onPress={pickImage}/>
+            <Button style={styles.button} title="העלה תמונות" onPress={upload}/>
 
-            <Button title="העלה תמונות" onPress={upload}/>
-
-
-            <View style={styles.imageList}>
-                {imageList &&
-                    imageList.map((img) => (
-                        <Image key={img.uri} source={{uri: img.uri}} style={styles.image}/>
-                    ))}
-            </View>
+            <Button style={styles.button} title="בחר תמונות" onPress={pickImage}/>
         </View>
-    );
+        <View style={styles.imageList}>
+            {imageList && imageList.map((img) => (<Image key={img.uri} source={{uri: img.uri}} style={styles.image}/>))}
+        </View>
+    </View>);
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
+        flex: 1, margin: 10, alignItems: 'center', justifyContent: 'center',
     },
-    imageList: {
-        marginTop: 20,
+    containerLong: {
         flexDirection: 'row',
-        flexWrap: 'wrap',
-        justifyContent: 'center',
+        height: 30,
+        justifyContent: 'space-between',
+        margin: 10,
+        alignItems: 'center',
     },
-    image: {
-        width: 100,
-        height: 100,
-        margin: 5,
+    button: {
+        margin: 10, width: '20%',
+    }, imageList: {
+        marginTop: 20, flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center',
+    }, image: {
+        width: 100, height: 100, margin: 5, borderRadius: 5,
     },
 });
